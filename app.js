9bbe1a1f481e5ea3e954d48b35f7373be83689e2
RAJ Mishra <rajnandanmishra2002@gmail.com>
2022-03-06 11:26:03 +0530

    commite

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+setInterval(function(){
+    let a=new Date();
+    document.querySelector(".time").innerHTML=a.toLocaleTimeString();
+},100)
+
+
+const bb=()=>{
+
+    fetch('https://animechan.vercel.app/api/random').then((user)=>{
+     return user.json();
+    }).then((message)=>{ 
+        document.querySelector('.txt').textContent=message.quote;
+    }).catch((e)=>{
+        document.querySelector('.txt').textContent='Error';
+    })
+    }
+
+    setInterval(bb,5000);
